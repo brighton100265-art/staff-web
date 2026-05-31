680398c434a3cfa7a4b10a2408d1c9ff9cd64f1d
brighton100265-art <brighton.100265@gmail.com>
2026-06-01 01:19:50 +0530

    Delete my-staff-web/api/save-leave.js

--- my-staff-web/api/save-leave.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { employee_id, leave_date } = req.body;
-    console.log("ได้รับข้อมูลแล้ว:", employee_id, leave_date);
-    return res.status(200).json({ status: "success" });
-  } else {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
-}
