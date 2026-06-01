736cc8024857b1f05bda8fe8651406320497ac99
brighton100265-art <brighton.100265@gmail.com>
2026-06-01 14:59:27 +0530

    test api

--- my-staff-web/api/save-leave.js
+++ my-staff-web/api/save-leave.js
@@ -1,23 +1,3 @@
-import { initializeApp, getApps } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  addDoc
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID
-};
-
-const app =
-  getApps().length
-    ? getApps()[0]
-    : initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
 export default async function handler(req, res) {
 
   if (req.method !== "POST") {
@@ -28,24 +8,9 @@ export default async function handler(req, res) {
 
   try {
 
-    const {
-      employee_id,
-      leave_date
-    } = req.body;
-
-    await addDoc(
-      collection(db, "leave_requests"),
-      {
-        employeeId: employee_id,
-        leaveDate: leave_date,
-        requestType: "LEAVE",
-        createdAt:
-          new Date().toISOString()
-      }
-    );
-
     return res.status(200).json({
-      status: "success"
+      status: "success",
+      received: req.body
     });
 
   } catch (e) {
